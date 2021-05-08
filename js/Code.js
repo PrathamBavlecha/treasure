795d78f09dfc789ec0acf68affd5bf29a72b8cf9
Pratham <pratham.bavlecha@gmail.com>
2021-05-08 18:52:26 +0530

    added third clue and inputs

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("ATADASBA",700,160)
+    fill("lightblue")
+    text("Hint: Storing things to remember", 700,180)
+    fill("white")
+    textSize(15)
+    text("CUTNIFON",400,260)
+    fill("lightblue")
+    text("Hint: Carrying out processes", 400,280)
+    
+    
+    
+}
